a4ab32cb723e9814892d92d46e47f4971d835d5d
west1 <email>
2023-04-18 17:58:26 +0400

    для сервера

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import flask
+import os
 import base64
 import requests
 import datetime as dt
@@ -273,4 +273,5 @@ def question():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
